40ee27b5f1715552e6d5cfc71ff7b30e21791f02
m5623skhj <m5623skhj@gmail.com>
2023-12-03 14:06:40 +0900

    * 파일 로드 성공 및 실패에 관한 출력 추가

--- ScriptToObject/ScriptToObject/JsonUtil.cpp
+++ ScriptToObject/ScriptToObject/JsonUtil.cpp
@@ -4,10 +4,13 @@
 #include "TestData.h"
 
 #define ADD_TO_CONTAINER(className, jsonFileName){ \
+	std::cout << "Start loading : " << jsonFileName << std::endl; \
 	if(AddToDataContainerFromJsonFile<className>(currentFolder + jsonScriptFolderPath + jsonFileName) == false) \
 	{ \
+		std::cout << "Load failed : " << jsonFileName << std::endl; \
 		return false; \
 	} \
+	std::cout << "Load completed : " << jsonFileName << std::endl; \
 }
 
 namespace JsonUtil
@@ -25,6 +28,10 @@ namespace JsonUtil
 
 		ADD_TO_CONTAINER(Test1, "Test1.json");
 		ADD_TO_CONTAINER(Test2, "Test2.json");
+
+		std::cout << std::endl 
+			<< "--------------------------------------" << std::endl 
+			<< "All files loaded successfully" << std::endl;
 
 		return true;
 	}
